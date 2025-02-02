4f244ed00c6c369e35845656e5caaa6efb11ea96
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-02-02 14:12:17 +0200

    EF_Core

---
+++ Demo/Context/EnterPriseDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Demo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Context
+{
+    internal class EnterPriseDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source =.;Database= EnterPrise; Login= Omar; Password=1234");
+            optionsBuilder.UseSqlServer("Server = .;Database= EnterPrise;Trusted_Connection= True");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
